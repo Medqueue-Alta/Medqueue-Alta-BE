7499801437a816d8d460243350afdc9c7cbfcb4e
fawwazer <mn,fawwaz99@gmail.com>
2024-04-04 15:20:19 +0700

    fix : error

--- main.go
+++ main.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"Medqueue-Alta-BE/config"
-	rd "Medqueue-Alta-BE/features/reservation/data"
-	rh "Medqueue-Alta-BE/features/reservation/handler"
-	rs "Medqueue-Alta-BE/features/reservation/services"
 	"Medqueue-Alta-BE/features/user/data"
 	"Medqueue-Alta-BE/features/user/handler"
 	"Medqueue-Alta-BE/features/user/services"
+	"Medqueue-Alta-BE/helper"
+	"Medqueue-Alta-BE/middlewares"
 	"Medqueue-Alta-BE/routes"
 
 	"github.com/labstack/echo/v4"
@@ -20,16 +19,12 @@ func main() {
 	db := config.InitSQL(cfg)  // konek DB
 
 	userData := data.New(db)
-	userService := services.NewService(userData)
+	userService := services.NewService(userData, helper.NewPasswordManager(), middlewares.NewMidlewareJWT())
 	userHandler := handler.NewUserHandler(userService)
-
-	reservationData := rd.New(db)
-	reservationService := rs.NewTodoService(reservationData)
-	reservationHandler := rh.NewHandler(reservationService)
 
 	e.Pre(middleware.RemoveTrailingSlash())
 	e.Use(middleware.Logger())
-	e.Use(middleware.CORS()) 
-	routes.InitRoute(e, userHandler, reservationHandler)
+	e.Use(middleware.CORS())
+	routes.InitRoute(e, userHandler)
 	e.Logger.Fatal(e.Start(":1323"))
 }
