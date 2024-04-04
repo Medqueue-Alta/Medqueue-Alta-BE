611506e500e2e8e21883d33e4bc0993279c7bb6b
fawwazer <mn,fawwaz99@gmail.com>
2024-04-04 15:09:58 +0700

    fix : temp delete reservasi

--- features/reservation/handler/request.go
+++
@@ -1,9 +0,0 @@
-package handler
-
-type ReservationRequest struct {
-	PoliKlinik   string `json:"poli_klinik" form:"poli_klinik"`
-	Hari         string `json:"hari" form:"hari"`
-	WaktuMulai   string `json:"waktumulai" form:"waktumulai"`
-	WaktuSelesai string `json:"waktuselesai" form:"waktuselesai"`
-	Kuota        int    `json:"kuota" form:"kuota"`
-}
